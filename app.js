2e9e6577aa8671edb0af117fb22c0625c1a01b8d
jdethington <jdethington@gmail.com>
2026-08-29 14:28:26 -0700

    Setup eslint and eslint:fix

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res
-    .status(200)
-    .json({ message: "Server is running. Welcome to the API!" });
+  return res.status(200).json({ message: "Server is running." });
 });
 
 export default app;
