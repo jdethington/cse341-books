34a7ae11aa9470b91b32d8a8f61c452e1ad579de
jdethington <jdethington@gmail.com>
2026-08-29 11:53:49 -0700

    Set up  Node.js and Express

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res
+    .status(200)
+    .json({ message: "Server is running. Welcome to the API!" });
+});
+
+export default app;
