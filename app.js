ba848707c9e90cabe37d38f0068506dce050ecf6
jdethington <jdethington@gmail.com>
2026-08-31 14:59:41 -0700

    Added Get /books route

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getDb } from "./src/db/connect.js";
+import router from "./src/router.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(router);
 
 app.get("/", (req, res) => {
   return res.status(200).json({ message: "Server is running." });
